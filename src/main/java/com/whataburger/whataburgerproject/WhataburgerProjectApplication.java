aefe33fea5c279de504f7d3b596c7bbf69206326
wndyd <wndyd0131@naver.com>
2024-04-17 02:53:04 -0500

    first commit

---
+++ src/main/java/com/whataburger/whataburgerproject/WhataburgerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.whataburger.whataburgerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhataburgerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhataburgerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whataburger/whataburgerproject/WhataburgerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whataburger.whataburgerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhataburgerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
