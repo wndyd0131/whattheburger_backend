5411e83b8845554e4c2bc1f6b940034a478dd871
wndyd <wndyd0131@naver.com>
2025-04-01 18:20:45 +0900

    add OptionController, OptionReadResponseDto

---
+++ src/main/java/com/whataburger/whataburgerproject/controller/dto/OptionReadResponseDto.java
@@ -0,0 +1,17 @@
+package com.whataburger.whataburgerproject.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class OptionReadResponseDto {
+    private Long optionId;
+    private String optionName;
+    private String imageSource;
+    private int optionCalories;
+}
