f12b1a1bcf39521834dc8bbb9ab711cc0fef5443
wndyd <wndyd0131@naver.com>
2026-05-15 15:11:40 +0900

    feat(inventory): add StoreInventory, Inventory, ProductOptionIngredient entities

--- src/main/java/com/whattheburger/backend/domain/Ingredient.java
+++ src/main/java/com/whattheburger/backend/domain/Ingredient.java
@@ -1,38 +1,23 @@
 package com.whattheburger.backend.domain;
 
+import com.whattheburger.backend.domain.enums.IngredientUnit;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ingredient {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ingredient_id")
     private Long id;
-//    @OneToMany(mappedBy = "ingredient")
-//    private List<ProductIngredient> productIngredient = new ArrayList<>();
     private String name;
-//    private Allergens
-//    private Nutrition
+    @Enumerated(EnumType.STRING)
+    private IngredientUnit unit;
 }
-
-
-/*
-===product===
-M:N
-Product - ProductIngredient - Ingredient
-whataburger
-===vegee===
-- lettuce
-- tomato
-- pickle
-- onion
-===meat===
-- meat petty
-===sauce===
-- mustard
-===bread===
-- bun
- */
